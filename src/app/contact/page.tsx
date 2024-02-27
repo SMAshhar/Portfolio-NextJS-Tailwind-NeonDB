75f10d2d333c6715a1d0c936d661882a6343986e
smashhar <syed.muhammad.ashhar@gmail.com>
2024-02-27 09:44:40 -0800

    testemonial section remains

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import ContactComponent from "@/components/Contact/Contact";
+
+export default function Contact () {
+    return (
+        <div className="w-full h-screen py-16 lg:flex items-center justify-center fixed bg-slate-900/80 bg-blend-color-dodge">
+            <ContactComponent />
+        </div>
+    )
+}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,31 @@
-export default function ProjectPage () {
+'use client'
+// components
+import WorkSlider from "./Slider";
+import Intro from "./Intro";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/variants";
+
+export default function ProjectPage() {
     return (
-        <div>
-            <h1>Project Page</h1>
+        <div className="flex flex-col xl:flex-row w-full xl:pr-24 items-center justify-center">
+            <motion.div
+                variants={fadeIn('right', 0.7)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="w-full px-2 py-4 flex items-center justify-center">
+                <Intro />
+            </motion.div>
+            <motion.div
+                variants={fadeIn('left', 0.7)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="w-full lg:w-2/3 xl:w-2/3 px-2 py-4 flex items-start justify-start">
+                <WorkSlider />
+            </motion.div>
         </div>
     )
 }
